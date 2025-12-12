b5ae9a3573eee09cccec6cedb8b841219e992c86
Ahmad Rashed <ahmad.rashed@alifsemi.com>
2025-12-11 21:14:51 -0800

    Cleanup of soc_rst.c, removal of legacy code.

--- libs/drv/soc_rst.c
+++ libs/drv/soc_rst.c
@@ -4,14 +4,9 @@
 #include "hostbase.h"
 #include "soc_rst.h"
 
-#if RTSS_BIN || MDM_BIN
-#include <string.h>
-#endif
-
 void reset_external_system0(uint32_t vtor_address)
 {
     *((volatile uint32_t *)0x1A604000) = 0;
-    *((volatile uint32_t *)0x1A605010) = vtor_address;
     *((volatile uint32_t *)0x1A605014) = vtor_address;
 
     /* apply reset & M55 CPUWAIT */
@@ -27,7 +22,6 @@ void reset_external_system0(uint32_t vtor_address)
 void reset_external_system1(uint32_t vtor_address)
 {
     *((volatile uint32_t *)0x1A604010) = 0;
-    *((volatile uint32_t *)0x1A60A020) = vtor_address;
     *((volatile uint32_t *)0x1A60A024) = vtor_address;
 
     /* apply reset & M55 CPUWAIT */
@@ -44,10 +38,8 @@ void release_external_system0()
 {
     HOSTBASE->EXT_SYS0_RST_CTRL = 0;
 
-#if SOC_FEAT_HAS_BULK_SRAM
     /* dummy read to force clocks to start */
     (void) *((volatile uint32_t *) APP_SRAM2_BASE);
-#endif
 }
 
 void release_external_system1()
@@ -57,26 +49,3 @@ void release_external_system1()
     /* dummy read to force clocks to start */
     (void) *((volatile uint32_t *) APP_SRAM4_BASE);
 }
-
-#if RTSS_BIN
-void sideload_external_system0() {
-    //uint32_t *rtsshp_loadaddr = (uint32_t *) SRAM0_BASE;
-    //uint32_t *rtsshp_loadaddr = (uint32_t *) SRAM1_BASE;
-    uint32_t *rtsshp_loadaddr = (uint32_t *) SRAM2_BASE;
-
-    /* dummy read to force clocks to start */
-    (void) *((volatile uint32_t *) SRAM2_BASE);
-    memcpy(rtsshp_loadaddr, rtsshp_bin, rtsshp_bin_size);
-}
-
-void sideload_external_system1() {
-    //uint32_t *rtsshe_loadaddr = (uint32_t *) SRAM0_BASE;
-    //uint32_t *rtsshe_loadaddr = (uint32_t *) SRAM1_BASE;
-    uint32_t *rtsshe_loadaddr = (uint32_t *) SRAM4_BASE;
-
-    /* dummy read to force clocks to start */
-    (void) *((volatile uint32_t *) SRAM4_BASE);
-    memcpy(rtsshe_loadaddr, rtsshe_bin, rtsshe_bin_size);
-}
-#endif
-
--- libs/drv/soc_rst.h
+++ libs/drv/soc_rst.h
@@ -1,8 +1,5 @@
 #include <stdint.h>
 
-#define RTSS_BIN    0
-#define MDM_BIN     0
-
 /* resetting the external system leaves it in the off state
  * vtor_address is where the cpu will start after release
  */
@@ -11,15 +8,3 @@ void reset_external_system1(uint32_t vtor_address);
 
 void release_external_system0();
 void release_external_system1();
-void release_m55m();
-void release_m55g();
-
-#ifdef RTSS_BIN
-void sideload_external_system0();
-void sideload_external_system1();
-#endif
-#ifdef MDM_BIN
-void sideload_m55m();
-void sideload_m55g();
-#endif
-
