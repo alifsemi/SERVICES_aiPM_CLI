6ee5ba6c3046025a3f3dddf1668f34971603a7e6
Ahmad Rashed <ahmad.rashed@alifsemi.com>
2026-08-05 16:00:56 -0700

    Fixed build error related to new MRAM_USER section

--- device/ensemble/RTE/Device/AE1C1F4051920PH0_M55_HE/app_mem_regions.h
+++ device/ensemble/RTE/Device/AE1C1F4051920PH0_M55_HE/app_mem_regions.h
@@ -21,10 +21,23 @@
 #define APP_MRAM_HE_BASE       0x80000000
 //   <o> RTSS HE Region size [bytes] <0x0-0x001CD000:8>
 //   <i> Defines size of RTSS HE application memory region.
-//   <i> Default: 0x0018C000
-#define APP_MRAM_HE_SIZE       0x001CD000
-
+//   <i> Default: 0x00100000
+#define APP_MRAM_HE_SIZE       0x00100000  //1 MB
 // </h>
+
+// <h>MRAM User Configuration
+// =======================
+//   <o> User Base address <0x80000000-0x801DCFFF:8>
+//   <i> Defines base address of MRAM user region
+#define APP_MRAM_USER_BASE    0x80100000
+
+//   <o> User Region size [bytes] <0x0-0x001DD000:8>
+//   <i> Defines size of MRAM user region
+#define APP_MRAM_USER_SIZE    0x000DD000  //884 KB
+
+// Application executable MRAM region (before USER area)
+#define APP_CODE_MRAM_SIZE  (APP_MRAM_USER_BASE - APP_MRAM_HE_BASE)
+//</h>
 
 // <h>RAM Configuration
 // =======================
